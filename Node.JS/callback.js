983d4149673a7a1b918b1241a791cfaea7df2e8e
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2020-08-25 14:17:36 +0530

    callback

---
+++ Node.JS/callback.js
@@ -0,0 +1,17 @@
+console.log("Before");
+
+getUser(5, (user) => {
+  console.log(user);
+});
+
+console.log("After");
+
+function getUser(id, cb) {
+  setTimeout(() => {
+    console.log("Calling DB...");
+    const user = { id: id, username: "UserName" };
+    // console.log("Result :", user);
+    // return user;
+    cb(user);
+  }, 3000);
+}
