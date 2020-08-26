db8c9c9fe8690dde144b1eb5ad30c3f7d1da882f
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2020-08-27 02:13:11 +0530

    mongoose

---
+++ Node.JS/express/mongoose.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const _const = require("./constant");
+const MONGODB_URI = process.env.MONGODB_URI || _const.MONGODB_URI;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+/* mongoose.connect() returns a Promise */
+mongoose.connect(MONGODB_URI, options, (err) => {
+  if (err) {
+    console.error("Unable to connect to db...", err.message);
+    throw new Error(err);
+  }
+  console.log("Successfully connected to db...");
+});
+
+/* //Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); */
