bd1deafc995d799feb2040ace440b2b330b3b8eb
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2020-08-28 18:35:28 +0530

    Mongoose Connection

--- Node.JS/express/mongoose.js
+++ Node.JS/express/mongoose.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
-const _const = require("./constant");
-const MONGODB_URI = process.env.MONGODB_URI || _const.MONGODB_URI || "mongodb://localhost/DB_NAME";
+const Schema = mongoose.Schema;
+const { MONGODB_URI, DB_NAME } = require("./constant");
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: DB_NAME,
 };
+
+console.log("Connecting...");
 /* mongoose.connect() returns a Promise */
 mongoose.connect(MONGODB_URI, options, (err) => {
   if (err) {
-    console.error("Unable to connect to db...", err.message);
+    console.error("⚠️! Unable to connect to db...", err.message);
     throw new Error(err);
   }
   console.log("Successfully connected to db...");
@@ -19,3 +22,33 @@ mongoose.connect(MONGODB_URI, options, (err) => {
 var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:')); */
+
+const courseSchema = new Schema({
+  name: String,
+  topics: [String],
+  isAvailable: Boolean,
+  created: { type: Date, default: Date.now() },
+  updated: { type: Date, default: Date.now() },
+});
+const Course = mongoose.model("Course", courseSchema);
+
+const course = new Course({
+  name: "Node.js Course",
+  topics: ["node.js", "node", "backend"],
+  isAvailable: true,
+});
+
+course.save((err, res) => {
+  if (err) throw new Error(err);
+  console.log("Document saved!");
+  console.log("Response :", res);
+  process.exit(0);
+});
+
+/* Close connection */
+/* mongoose.connection.close((err) => {
+  if (err) console.log(err);
+  console.log("Connection closed");
+  process.exit(0);
+});
+ */
