332e576f4076e1609439fc9ad25e31b5d1fb0643
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2021-06-15 14:19:26 +0530

    dependabot

--- Node.JS/Express/app.js
+++ Node.JS/Express/app.js
@@ -1,30 +1,23 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const morgan = require("morgan");
-const logger = require("./logger");
-const router = require("./routes/Router");
-const genreRouter = require("./routes/Genres");
-const customerRouter = require("./routes/Customers");
-const DBConnector = require("./dao/DBConnector");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const logger = require('./logger');
+const router = require('./routes/Router');
+const genreRouter = require('./routes/Genres');
+const customerRouter = require('./routes/Customers');
+const DBConnector = require('./dao/DBConnector');
 
 const app = express();
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true })); // initialize body-parser to parse incoming parameters requests to req.body
 app.use(cookieParser()); // initialize cookie-parser to allow us access the cookies stored in the browser
 
 app.use(logger); // custom middleware
 
-/* Use logger in specific env */
-/* if (app.get("env") === "development") {
-  app.use(morgan("short"));
-  console.log("Morgan enabled...");
-} */
-
-app.use("/", router);
-app.use("/api/genres", genreRouter);
-app.use("/api/customers", customerRouter);
+app.use('/', router);
+app.use('/api/genres', genreRouter);
+app.use('/api/customers', customerRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
