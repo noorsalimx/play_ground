d0907068c950e7d603fae4d002058c52cdc81bc9
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2021-10-24 17:19:39 +0530

    web

---
+++ web/Card/js/chart.js
@@ -0,0 +1,22 @@
+var ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "pie",
+  data: {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
+        hoverOffset: 4,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
