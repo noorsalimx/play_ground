663923bf4078d35cb8fe1a08c6bcc139339e15c4
Noor Salim <24814579+mrlazyg@users.noreply.github.com>
2021-12-20 21:57:59 +0530

    cb

--- nodejs/callback.js
+++ nodejs/callback.js
@@ -1,15 +1,15 @@
-console.log("Before");
+console.log('Before');
 
 getUser(5, (user) => {
   console.log(user);
 });
 
-console.log("After");
+console.log('After');
 
 function getUser(id, cb) {
   setTimeout(() => {
-    console.log("Calling DB...");
-    const user = { id: id, username: "UserName" };
+    console.log('Calling DB...');
+    const user = { id: id, username: 'UserName' };
     // console.log("Result :", user);
     // return user;
     cb(user);
